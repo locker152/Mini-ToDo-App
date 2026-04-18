f5cd1b6eac0ea355e59ff1150c13f4ac85cf37d9
locker152 <2160874009@qq.com>
2026-04-18 15:09:39 +0800

    增加了删除功能，并去除了初始任务使功能更加完善

--- minitodoapp-js.js
+++ minitodoapp-js.js
@@ -1,32 +1,40 @@
-let newid = 1
+var newid = 1
 function addTask() {
     var taskname = document.getElementById('input-class')
 
-var newtask = taskname.value.trim()
-if (newtask === '') {
-    alert('请输入任务名称')
-    return
-}
+    var newtask = taskname.value.trim()
+    if (newtask === '') {
+        alert('请输入任务名称')
+        return
+    }
 
+    var table = document.getElementById('table-element')
+    var tbody = table.getElementsByTagName('tbody')[0]
 
-var table = document.getElementById('table-element')
-var tbody = table.getElementsByTagName('tbody')[0]
+    var newrow = tbody.insertRow(-1)
 
-var newrow = tbody.insertRow(-1)
+    var cell1 = newrow.insertCell(0)
+    var cell2 = newrow.insertCell(1)
+    var cell3 = newrow.insertCell(2)   
 
+    cell1.innerHTML = newtask
+    cell2.innerHTML = `<input id="complete_${newid}" type="radio" name="choice_${newid}">完成 <input id="incomplete_${newid}" type="radio" name="choice_${newid}">未完成`
+    
+    
+    var deleteBtn = document.createElement('button')
+    deleteBtn.textContent = '删除'
+    deleteBtn.className = 'delete-btn'   
+    deleteBtn.onclick = function() {
+        newrow.remove()
+    }
+    cell3.appendChild(deleteBtn)
+    
 
-var cell1 = newrow.insertCell(0)
-var cell2 = newrow.insertCell(1)
+    newid++
 
-cell1.innerHTML = newtask
-cell2.innerHTML = `<input type="radio" name="choice_${newid}">完成 <input type="radio" name="choice_${newid}">未完成`
+    cell2.className = "state"
 
-newid++
-cell2.className = "state"
-var newtask = taskname.value.trim()
-taskname.value = "";
+    
+    taskname.value = "";
     taskname.focus();
 }
-
-
-
